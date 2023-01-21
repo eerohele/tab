9e45146b26b184844ee4a819a38d80a5dd191422
Eero Helenius <eero.helenius@pm.me>
2023-01-21 16:31:20 +0200

    Refactor tab.db

--- src/tab/db.clj
+++ src/tab/db.clj
@@ -1,43 +1,41 @@
 (ns tab.db
+  "Tab's in-memory database."
   (:import (java.util UUID)))
 
 (defn uuid
+  "Return a UUID."
   []
   (UUID/randomUUID))
 
 (defn pristine
+  "Return an empty database."
   []
   (atom {}))
 
 (defn evacuate!
+  "Given a database, remove everything in the database.
+
+  Return the database."
   [db]
   (reset! db {})
   db)
 
 (defn put!
+  "Given a database and a value, put the value into the database."
   [db val]
   (let [id (uuid)]
     (swap! db assoc id val)
     id))
 
-(defn evict!
-  [db id]
-  (swap! db dissoc id))
-
 (defn pull
+  "Given a database and an ID, pull the value with the given ID from the
+  database."
   [db id]
   (get @db id))
-
-(defn extract!
-  [db id]
-  (let [data (pull db id)]
-    (evict! db id)
-    data))
 
 (comment
   (def db (pristine))
   (def uuid (put! db {:a 1}))
   (deref db)
-  (extract! db uuid)
-  (deref db)
+  (pull db uuid)
   ,,,)

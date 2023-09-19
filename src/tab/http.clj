a28ca634a992a910ff18822a099417da82b14178
Eero Helenius <eero.helenius@yle.fi>
2023-09-19 21:48:41 +0300

    Use singleThreadExecutor instead of future

--- src/tab/http.clj
+++ src/tab/http.clj
@@ -7,7 +7,7 @@
   (:import (java.io BufferedOutputStream BufferedReader InputStreamReader)
            (java.net InetAddress ServerSocket SocketException)
            (java.nio.charset StandardCharsets)
-           (java.util.concurrent BlockingQueue Executors ExecutorService TimeUnit)
+           (java.util.concurrent BlockingQueue Executors TimeUnit)
            (java.util UUID)))
 
 (set! *warn-on-reflection* true)
@@ -16,11 +16,6 @@
   (address [this] "Given a HttpServer, return the address it listens on.")
   (broadcast [this event] "Send a server-sent event (SSE) to all connected clients.")
   (halt [this] "Halt a HttpServer."))
-
-(defn ^:private make-request-thread-pool
-  ^ExecutorService []
-  (let [thread-pool-size (-> (Runtime/getRuntime) .availableProcessors inc)]
-    (Executors/newFixedThreadPool thread-pool-size (thread/make-factory :name-suffix :request))))
 
 (defn serve
   "Start a HTTP server.
@@ -42,7 +37,9 @@
          sse-heartbeat-frequency-secs 10}}]
   (let [server-id (UUID/randomUUID)
         ^ServerSocket socket (ServerSocket. port 0 (InetAddress/getLoopbackAddress))
-        request-thread-pool (make-request-thread-pool)
+        accept-loop-thread-pool (Executors/newSingleThreadExecutor (thread/make-factory :name-suffix :accept-loop))
+        thread-pool-size (-> (Runtime/getRuntime) .availableProcessors inc)
+        request-thread-pool (Executors/newFixedThreadPool thread-pool-size (thread/make-factory :name-suffix :request))
         heartbeat-thread-pool (Executors/newScheduledThreadPool 1 (thread/make-factory :name-suffix :heartbeat :ex-log-level :fine))
         queue-thread-pool (Executors/newCachedThreadPool (thread/make-factory :name-suffix :queue))
 
@@ -51,103 +48,102 @@
 
         address
         (let [^java.net.InetSocketAddress address (.getLocalSocketAddress socket)]
-          (format "http://%s:%s" (.getHostString address) (.getLocalPort socket)))
+          (format "http://%s:%s" (.getHostString address) (.getLocalPort socket)))]
 
-        accept-loop
-        (future
-          (try
-            (loop []
-              (let [client (.accept socket)
-                    remote-addr (str (.getRemoteSocketAddress client))]
-                (log/log :fine {:event :accept-client :remote-addr remote-addr})
+    (thread/exec accept-loop-thread-pool
+      (try
+        (loop []
+          (let [client (.accept socket)
+                remote-addr (str (.getRemoteSocketAddress client))]
+            (log/log :fine {:event :accept-client :remote-addr remote-addr})
 
-                (thread/exec request-thread-pool
-                  (let [reader (-> client .getInputStream (InputStreamReader. StandardCharsets/UTF_8) BufferedReader.)
+            (thread/exec request-thread-pool
+              (let [reader (-> client .getInputStream (InputStreamReader. StandardCharsets/UTF_8) BufferedReader.)
 
-                        lines (not-empty
-                                (doall
-                                  (take-while (fn [^String line] (and line (not (.isBlank line))))
-                                    (repeatedly #(.readLine reader)))))
+                    lines (not-empty
+                            (doall
+                              (take-while (fn [^String line] (and line (not (.isBlank line))))
+                                (repeatedly #(.readLine reader)))))
 
-                        request (ring/parse-request lines)
+                    request (ring/parse-request lines)
 
-                        output-stream (-> client .getOutputStream BufferedOutputStream.)
+                    output-stream (-> client .getOutputStream BufferedOutputStream.)
 
-                        close (fn []
-                                (try
-                                  (.close client)
-                                  (catch SocketException _
-                                    (log/log :fine {:event :socket-close-failed :remote-addr remote-addr}))))
-
-                        response
-                        (try
-                          (handler (assoc request :remote-addr remote-addr :server-id server-id))
-                          (catch Throwable ex
-                            (log/log :severe {:event :write-response-failed :ex ex})
-                            {:status 500
-                             :headers {"Content-Type" "text/html"}
-                             :body (html/page
-                                     (template/error-page request
-                                       ($ :h1 "I messed up.")
-                                       ($ :p "Sorry. "
-                                         ($ :a {:href "https://github.com/eerohele/tab/issues"} "File an issue?"))))}))]
-
-                    (try
-                      (ring/write-response output-stream response)
-
-                      (if (= (get-in response [:headers "Content-Type"]) "text/event-stream")
-                        (let [event-queue (:body response)
-                              _ (swap! !queues conj event-queue)
-                              evict-queue! (fn [] (swap! !queues disj event-queue))]
-                          (log/log :fine
-                            {:event :accept-queue
-                             :remote-addr remote-addr
-                             :connected-clients (count-queues)})
-
-                          (.scheduleAtFixedRate heartbeat-thread-pool
-                            (fn []
-                              (try
-                                (log/log :fine
-                                  {:event :send-heartbeat
-                                   :remote-addr remote-addr
-                                   :connected-clients (count-queues)})
-                                (.write output-stream (.getBytes ":\n\n" StandardCharsets/UTF_8))
-                                (.flush output-stream)
-                                (catch SocketException ex
-                                  (evict-queue!)
-                                  (log/log :fine
-                                    {:event :evict-queue/send-heartbeat-failed
-                                     :remote-addr remote-addr
-                                     :connected-clients (count-queues)})
-                                  ;; rethrow to cancel scheduled task
-                                  (throw ex))))
-                            sse-heartbeat-initial-delay-secs
-                            sse-heartbeat-frequency-secs
-                            TimeUnit/SECONDS)
-
-                          (thread/exec queue-thread-pool
+                    close (fn []
                             (try
-                              (loop []
-                                (let [^String item (.take ^BlockingQueue event-queue)]
-                                  (when-not (identical? item ::quit)
-                                    (.write output-stream (.getBytes item StandardCharsets/UTF_8))
-                                    (.flush output-stream)
-                                    (recur))))
+                              (.close client)
                               (catch SocketException _
-                                (log/log :fine
-                                  {:event :evict-queue/queue-write-failed
-                                   :remote-addr remote-addr
-                                   :connected-clients (count-queues)}))
-                              (finally
-                                (evict-queue!)
-                                (close)))))
-                        (close))
-                      (catch SocketException ex
-                        (log/log :fine {:event :write-response-failed :ex ex}))))))
-              (recur))
-            (catch SocketException ex
-              (log/log :fine {:event :client-disappeared? :ex ex})
-              :halted)))]
+                                (log/log :fine {:event :socket-close-failed :remote-addr remote-addr}))))
+
+                    response
+                    (try
+                      (handler (assoc request :remote-addr remote-addr :server-id server-id))
+                      (catch Throwable ex
+                        (log/log :severe {:event :write-response-failed :ex ex})
+                        {:status 500
+                         :headers {"Content-Type" "text/html"}
+                         :body (html/page
+                                 (template/error-page request
+                                   ($ :h1 "I messed up.")
+                                   ($ :p "Sorry. "
+                                     ($ :a {:href "https://github.com/eerohele/tab/issues"} "File an issue?"))))}))]
+
+                (try
+                  (ring/write-response output-stream response)
+
+                  (if (= (get-in response [:headers "Content-Type"]) "text/event-stream")
+                    (let [event-queue (:body response)
+                          _ (swap! !queues conj event-queue)
+                          evict-queue! (fn [] (swap! !queues disj event-queue))]
+                      (log/log :fine
+                        {:event :accept-queue
+                         :remote-addr remote-addr
+                         :connected-clients (count-queues)})
+
+                      (.scheduleAtFixedRate heartbeat-thread-pool
+                        (fn []
+                          (try
+                            (log/log :fine
+                              {:event :send-heartbeat
+                               :remote-addr remote-addr
+                               :connected-clients (count-queues)})
+                            (.write output-stream (.getBytes ":\n\n" StandardCharsets/UTF_8))
+                            (.flush output-stream)
+                            (catch SocketException ex
+                              (evict-queue!)
+                              (log/log :fine
+                                {:event :evict-queue/send-heartbeat-failed
+                                 :remote-addr remote-addr
+                                 :connected-clients (count-queues)})
+                              ;; rethrow to cancel scheduled task
+                              (throw ex))))
+                        sse-heartbeat-initial-delay-secs
+                        sse-heartbeat-frequency-secs
+                        TimeUnit/SECONDS)
+
+                      (thread/exec queue-thread-pool
+                        (try
+                          (loop []
+                            (let [^String item (.take ^BlockingQueue event-queue)]
+                              (when-not (identical? item ::quit)
+                                (.write output-stream (.getBytes item StandardCharsets/UTF_8))
+                                (.flush output-stream)
+                                (recur))))
+                          (catch SocketException _
+                            (log/log :fine
+                              {:event :evict-queue/queue-write-failed
+                               :remote-addr remote-addr
+                               :connected-clients (count-queues)}))
+                          (finally
+                            (evict-queue!)
+                            (close)))))
+                    (close))
+                  (catch SocketException ex
+                    (log/log :fine {:event :write-response-failed :ex ex}))))))
+          (recur))
+        (catch SocketException ex
+          (log/log :fine {:event :client-disappeared? :ex ex})
+          :halted)))
 
     (reify HttpServer
       (address [_] address)
@@ -162,5 +158,5 @@
         (.shutdown request-thread-pool)
         (.shutdown heartbeat-thread-pool)
         (.shutdownNow queue-thread-pool)
-        (future-cancel accept-loop)
+        (.shutdown accept-loop-thread-pool)
         (.close socket)))))

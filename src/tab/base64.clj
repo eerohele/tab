e78061adfed17a5d7f0536e2fdd32ec544797364
Eero Helenius <eero.helenius@yle.fi>
2023-09-21 23:28:03 +0300

    Add tab.base64/wrap

--- src/tab/base64.clj
+++ src/tab/base64.clj
@@ -1,6 +1,7 @@
 (ns tab.base64
   "Base64 utilities."
-  (:import (java.nio.charset StandardCharsets)
+  (:import (java.io OutputStream)
+           (java.nio.charset StandardCharsets)
            (java.util Base64 Base64$Encoder)))
 
 (set! *warn-on-reflection* true)
@@ -11,3 +12,8 @@
   "Given a string, return a Base64-encoded version of that string."
   [^String s]
   (.encodeToString encoder (.getBytes s StandardCharsets/UTF_8)))
+
+(defn wrap
+  "Given a java.io.OutputStream, wrap the OutputStream for encoding byte data."
+  ^OutputStream [output-stream]
+  (.wrap encoder output-stream))

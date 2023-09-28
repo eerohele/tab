ec8a3f2f863011fc570fd9c92d62be33abdb564a
Eero Helenius <eero.helenius@yle.fi>
2023-09-28 11:30:04 +0300

    WIP

--- src/tab/impl/pprint.clj
+++ src/tab/impl/pprint.clj
@@ -1,3 +1,14 @@
+;; Copyright (c) Eero Helenius. All rights reserved.
+;;
+;; The use and distribution terms for this software are covered by the
+;; MIT License (https://mit-license.org), which can be found in the file
+;; named LICENSE at the root of this distribution.
+;;
+;; By using this software in any fashion, you are agreeing to be bound
+;; by the terms of this license.
+;;
+;; You must not remove this notice, or any other, from this software.
+
 (ns tab.impl.pprint
   "A pretty-printer for Clojure data structures."
   {:author "Eero Helenius"}
